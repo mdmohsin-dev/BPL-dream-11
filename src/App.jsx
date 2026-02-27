27fca88c5613430800c008c538a0fb8ef4b336be
md mohosin <mdmohosin.web@gmail.com>
2026-02-28 00:43:22 +0600

    create navbar

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./components/Navbar"
+
+function App() {
+
+  return (
+    <div className="max-w-330 w-full mx-auto">
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import logo from "../assets/images/logo.png"
+import coinLogo from "../assets/images/dollar.png"
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex="-1"
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li><a>Item 1</a></li>
+                        <li>
+                            <a>Parent</a>
+                            <ul className="p-2">
+                                <li><a>Submenu 1</a></li>
+                                <li><a>Submenu 2</a></li>
+                            </ul>
+                        </li>
+                        <li><a>Item 3</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <img src={logo} alt="" />
+                </div>
+            </div>
+            <div className="navbar-end ">
+                <div className="hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li className="text-[16px]"><a>Home</a></li>
+                        <li className="text-[16px]"><a>Fixture</a></li>
+                        <li className="text-[16px]"><a>Teams</a></li>
+                        <li className="text-[16px]"><a>Schedules</a></li>
+                    </ul>
+                </div>
+                <button className="btn">0 coin <img src={coinLogo} alt="" /></button>
+            </div>
+
+        </div>
+    );
+};
+
+export default Navbar;

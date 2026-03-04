907ab1f91d825b36b1397d290514f14333056064
md mohosin <mdmohosin.web@gmail.com>
2026-03-04 11:43:21 +0600

    change json data

--- src/components/Player/Players.jsx
+++ src/components/Player/Players.jsx
@@ -1,26 +1,21 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Player from "./Player";
 
-const playersPromis = fetch("players.json").then(res => res.json())
+const Players = ({ handleSelectPlayer }) => {
 
-const Players = ({ handleSelectPlayer, }) => {
+    const [players, setPlayers] = useState([])
 
-    // const [players, setPlayers] = useState([])
-
-    // useEffect(() => {
-    //     fetch('players.json')
-    //         .then(res => res.json())
-    //         .then(data => setPlayers(data))
-    // }, [])
-
-
-    const players = use(playersPromis)
+    useEffect(() => {
+        fetch('players.json')
+            .then(res => res.json())
+            .then(data => setPlayers(data))
+    }, [])
 
     return (
         <div className='mt-8 text-black'>
             <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
                 {
-                    players.map(player => <Player player={player} handleSelectPlayer={handleSelectPlayer}></Player>)
+                    players.map(player => <Player key={player.player_id} player={player} handleSelectPlayer={handleSelectPlayer}></Player>)
                 }
             </div>
         </div>
